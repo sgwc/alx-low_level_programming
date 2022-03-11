5b115cdb4c914e53fe25b40bcf506388e45bc9ec
Samson <et980.0260@yahoo.com>
2022-03-11 07:11:07 -0800

    negative zero positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - prints random numbers with its sign
- * Retrun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -14,14 +14,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if ( n < 0 )
-		printf("%d is negative\n",n);
+	if (n < 0)
+		printf("%d is negative\n", n);
 	else if ( n == 0)
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	else
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 
 	return (0);
 
-	
 }
