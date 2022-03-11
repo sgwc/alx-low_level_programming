d39ea53a387560e30626f9a00955bc5548e71e1b
Samson <et980.0260@yahoo.com>
2022-03-11 14:45:34 -0800

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * Description: print the combination of all the single digits
+ * Starting from 0, followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i ,j;
+	
+	for(i = 0; i <= 9; i++)
+	{
+		for(j = i + 1; j <= 9; j++)
+		{
+				putchar(48 + i);
+				putchar(48 + j);
+				if (i >= 8 && j >= 9)
+					break;
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+
+		
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
