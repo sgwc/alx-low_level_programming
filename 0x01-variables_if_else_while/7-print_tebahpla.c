872f0c51fea4e579060a5f768e8c76eb058aef3c
Samson <et980.0260@yahoo.com>
2022-03-11 10:20:39 -0800

    reverse alphabet5

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all lower case alphabet in the stdout
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+
+}
