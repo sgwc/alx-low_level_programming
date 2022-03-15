ee6349cbd0879cf132a431e66f7ae27dc499a3a1
Samson <et980.0260@yahoo.com>
2022-03-15 15:05:49 -0700

    checks sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - determine the absolute value of n
+ *@n : char argument
+ * Return - 1 success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
