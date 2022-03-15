9c7690179590bffb1ffdd0de35a8481cd8e02288
Samson <et980.0260@yahoo.com>
2022-03-15 14:49:03 -0700

    checks sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - print sign of the given interger
- * @n : interger number
+ * @n : char whose signs are printed
  * Return : 1 if n is greater than 0 and 0 if n is 0 and
  */
 int print_sign(int n)
