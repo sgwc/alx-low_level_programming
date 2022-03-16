aa61745a69dbb617dc997f04fa906b07146ea8a1
Samson <et980.0260@yahoo.com>
2022-03-15 17:03:35 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - prints time table of 9
+ *
+ * Return : Always 0
+ */
+void times_table(void)
+{
+	int i, j, p;
+	for(i = 0; i <= 9; i++)
+	{
+		for(j = 0; j <= 9; j++)
+		{
+			p = i * j;
+			if (j == 0)
+			{
+				_putchar(p + '0');
+			}
+			else
+			{
+				if (p < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p / 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
