2d536a745859764aca83cf46c4504ca60919d66e
Samson <et980.0260@yahoo.com>
2022-03-15 17:05:25 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,10 @@
 void times_table(void)
 {
 	int i, j, p;
-	for(i = 0; i <= 9; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			p = i * j;
 			if (j == 0)
