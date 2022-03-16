0705d578a8884f67a6bbd5649f6c0f28993e9be1
Samson <et980.0260@yahoo.com>
2022-03-15 17:16:49 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,13 +3,14 @@
 /**
  * add - adds two integers
  *@a:the first operand
- @b: the seconde operand
+ *@b: the seconde operand
  * Return: the sum of two numbers
  */
 
 int add(int a, int b)
 {
 	int sum;
+
 	sum = a + b;
 	return (sum);
 }
