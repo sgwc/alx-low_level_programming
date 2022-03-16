961cb555a43ef27da7f4d1465d15f163e9a84552
Samson <et980.0260@yahoo.com>
2022-03-15 23:52:33 -0700

    print nmb upto 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural number up to 98 to the stdout
+ * @n : natural number;
+ * Return: Always 0;
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if ( i != 98)
+			{
+				printf("%d", i);
+				printf(", ");
+			}
+			else
+				printf("%d", i);
+		}
+		printf("\n");
+	}
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if ( i != 98)
+			{
+				printf("%d", i);
+				printf(", ");
+			}
+			else
+				printf("%d", i);
+		}
+		printf("\n");
+	}
+	else
+		printf("%d\n", n);
+}
