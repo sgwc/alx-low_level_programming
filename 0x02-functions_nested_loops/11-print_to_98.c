1711620e1e36eb66f818f159894b43fab1c8729c
Samson <et980.0260@yahoo.com>
2022-03-15 23:56:06 -0700

    print nmb upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			if ( i != 98)
+			if (i != 98)
 			{
 				printf("%d", i);
 				printf(", ");
@@ -28,7 +28,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if ( i != 98)
+			if (i != 98)
 			{
 				printf("%d", i);
 				printf(", ");
