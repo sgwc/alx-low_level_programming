72ea8df798c0b38700f89d43e71a184139422b90
sgwc <45393676+sgwc@users.noreply.github.com>
2022-03-18 23:30:40 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,12 @@ int main(void)
 		else if(((i % 3) == 0) && !((i % 5) == 0))
 			printf("Fizz ");
 		else if((!(i % 3) == 0) && ((i % 5) == 0))
-			printf("Buzz ");
+		{	printf("Buzz");
+			if (i != 100)
+			{
+				printf(" ");
+			}
+		}
 		else
 			printf("%d ", i);
 	}
