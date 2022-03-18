8aac967576d6b7ced1ed8d1637fa1c5926379799
sgwc <45393676+sgwc@users.noreply.github.com>
2022-03-18 22:21:18 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - list all digits form 0 - 9
- * 
- * Return: Always 0
+ * print_numbers - print from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	int i;
-	
+
 	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
