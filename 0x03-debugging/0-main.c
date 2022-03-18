e717f42bf72749b672f6ac869d0d67b1093832b0
sgwc <45393676+sgwc@users.noreply.github.com>
2022-03-18 22:01:10 +0300

    update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - test function that prints if integer is positive or negative
- * Return: 0 on success
+ * Return: 0
  */
 
 int main(void)
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
