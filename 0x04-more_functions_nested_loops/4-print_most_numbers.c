59c059bb8b952a601afdaf3f471874af08b3d176
Samson <et980.0260@yahoo.com>
2022-03-18 00:14:46 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - list all digits form 0 - 9
+ * print_most_numbers - list all digits form 0 - 9, except 2 and 4
  * 
  * Return: Always 0
  */
@@ -9,7 +9,6 @@
 void print_most_numbers(void)
 {
 	int i;
-	
 	for (i = 48; i <= 57; i++)
 	{
 		if (i != 50 && i != 52)
