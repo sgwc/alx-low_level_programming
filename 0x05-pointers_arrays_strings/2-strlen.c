651312ff9868b2bfa96e59c5a66a23b80123cfa2
Samson <et980.0260@yahoo.com>
2022-03-21 11:01:44 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the lengh of a string variable
+ * @s: string variable
+ * Return: the length of the s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while( s[len] != '\0')
+	{
+		len++;
+
+	}
+	return len;
+}
