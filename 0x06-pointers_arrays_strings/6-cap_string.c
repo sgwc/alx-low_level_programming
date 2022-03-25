bcb2a19ad14745cec6e9df401d1246ab49e1d55c
sgwc <45393676+sgwc@users.noreply.github.com>
2022-03-25 04:29:38 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,31 @@
 #include "main.h"
-
 /**
- * cap_string - Capitalizess all words of the given string
- * @a: stirng input
- * Return: string
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *cap_string(char *a)
-{
-	int i;
 
-	if (a[0] >= 97 && a[0] <= 122)
-		a[0] = a[0] - 32;
-	for (i =  1; a[i] != '\0'; i++)
-	{
-		if (a[i - 1] == 32 || a[i - 1] == '\t' || a[i - 1] == '\n' || a[i - 1] == 44 || a[i - 1] == 46 || a[i - 1] == 59 || a[i - 1] == 33 || a[i - 1] == 63 || a[i - 1] == 34 || a[i - 1] == 40 || a[i - 1] == 41 || a[i - 1] == 123 || a[i - 1] == 125)
-		{
-			if(a[i] >= 97 && a[i] <= 122)
-				a[i] = a[i] - 32;
-		}
-	}
-	return (a);
+
+char *cap_string(char *s)
+{
+int count = 0, i;
+int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == separators[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
+}
+}
+count++;
+}
+return (s);
 }
