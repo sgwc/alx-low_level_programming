d5f1b17dd79e0b3127447d3a143e739a29f076f9
sgwc <45393676+sgwc@users.noreply.github.com>
2022-03-25 06:07:15 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,24 +7,21 @@
 
 char *leet(char *s)
 {
-int count = 0, i;
-int lower_case[] = {97, 101, 111, 116, 108};
-int upper_case[] = {65, 69, 79, 84, 76};
-int numbers[] = {52, 51, 48, 55, 49};
-
-
-while (*(s + count) != '\0')
-{
-for (i = 0; i < 5; i++)
-{
-if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
-{
-*(s + count) = numbers[i];
-break;
-}
-}
-count++;
-}
-
-return (s);
+  int i, j;
+  int lower_case[] = {97, 101, 111, 116, 108};
+  int upper_case[] = {65, 69, 79, 84, 76};
+  int numbers[] = {52, 51, 48, 55, 49};
+  
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    for (j = 0; j < 5; j++)
+    {
+      if (s[i] == lower_case[j] || s[i] == upper_case[j])
+      {
+        s[i] = numbers[j];
+        break;
+      }
+    }
+  }
+  return (s);
 }
