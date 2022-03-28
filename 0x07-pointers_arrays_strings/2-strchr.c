904ee68d345ff3d3039f1c35f8b887a48b9880d1
Samson <et980.0260@yahoo.com>
2022-03-28 09:52:26 -0700

    locates a character in a sting

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - find the char c in s
+ * @s: input string
+ * @c: character to look for in s
+ *
+ * Return: first occorenc of
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i ;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (NULL);
+}
