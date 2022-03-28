861a88a1e871dd11fa198d174658e8c33c01845c
sgwc <45393676+sgwc@users.noreply.github.com>
2022-03-29 01:14:22 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,44 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring
- * @s: stirng input
- * @accept:the characters to lookfor
- *
- * Return: number of occurrences
- */
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ * Return: return count
+ **/
 
 unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int count;
-	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+{
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	i = 0;
+	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		j = 0;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
 		{
-			if (s[i] == accept[j])
-				count++;
+			if (str2[j] == str1[i]) /*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+			}
+
+			j++;    /*add j+1*/
 		}
+
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
 	}
-	return (count);
+
+	return (count); /*return the value of count*/
 }
