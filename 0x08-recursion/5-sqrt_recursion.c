8fdec66674c4c13c73180dc921be3636eee525c9
Samson <et980.0260@yahoo.com>
2022-03-30 22:45:26 -0700

    update 2-strlen_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculate square root
+ * @n: intger input
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	int sqrt;
+
+	sqrt = 1;
+	if ((sqrt * sqrt) != n)
+		sqrt = sqrt * _sqrt_recursi:wq
+			on();
+}
