fc1cd6a7e600c656be54d12dafe11c08a6699c7e
sgwc <45393676+sgwc@users.noreply.github.com>
2022-04-02 01:27:55 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,53 +1,35 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
- **/
-
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
-
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
+	int i, j, f, flag;
 
-	str1 = s;
-	str2 = accept;
+	f = 0;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[i] == s[j])
+			if (s[i] == accept[j])
 			{
-				count++;
-				break;
+				f++;
+				flag = 1;
 			}
-	i = 0;
-	while (str1[i] != '\0') /*Declaring WHILE *s */
-	{
-		j = 0;
-		while (str2[j] != '\0') /*Declaring WHILE *accept*/
-		{
-			if (str2[j] == str1[i]) /*Evaluate condition*/
-			{
-				count++; /*count number*/
-				break;
-			}
-
-			j++;    /*add j+1*/
 		}
-
-		if (s[i] != accept[j]) /*If aren't equals*/
+		if (flag == 0)
 		{
-			break;
+			return (f);
 		}
-
-		i++; /*add x+1*/
 	}
 
-	return (count); /*return the value of count*/
+	return (0);
 }
