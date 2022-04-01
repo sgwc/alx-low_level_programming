55e92a97d7f2be54fb40bc8a56950fe270e4cd6e
Samson <et980.0260@yahoo.com>
2022-04-01 10:24:30 -0700

    add to integer arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0(Success)
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+/**
+ * main - prints the multiplication of two integrs
+ * @argc:number of arguments
+ * @argv: Arguments passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	
+	for (i = 1; i < argc; i++)
+	{
+		if (check_num(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
