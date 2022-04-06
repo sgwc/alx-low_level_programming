f381db8ce9db01df38b13b701b4bfeba71c6df12
Samson <et980.0260@yahoo.com>
2022-04-05 19:06:27 -0700

    update 2-strconcat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,22 +19,17 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 		s2 = "";
-		size = strlen(s1) + strlen(s2) + 1;
 	}
 	else if (s1 == NULL)
 	{
 		s1 = "";
-		size = strlen(s1) + strlen(s2) + 1;
 	}
 	else if (s2 == NULL)
 	{
 		s2 = "";
-		size = strlen(s1) + strlen(s2) + 1;
 	}
-	else
-	{
-		size = strlen(s1) + strlen(s2) + 1;
-	}
+
+	size = strlen(s1) + strlen(s2) + 1;
 
 	s = (char *)malloc(size * sizeof(char));
 
