70fbff2571dea4fc550d9d6487cd88d05a1ca8f4
Samson <et980.0260@yahoo.com>
2022-04-07 11:30:00 -0700

    update 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocat memory block used for array
+ * @nmemb: array input
+ * @size: size of array
+ *
+ * Return: pointer to a address on success
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * sizeof(size));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(ptr, 0, size * sizeof(ptr[0]));
+
+	return (ptr);
+}
