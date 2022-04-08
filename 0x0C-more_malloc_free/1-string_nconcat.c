7eff6f1fb5d5c5cba4c426a68eb1379dd9739e3d
sgwc <45393676+sgwc@users.noreply.github.com>
2022-04-08 05:50:18 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,28 +1,41 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * _strlen - Length of string
+ * @s : string
+ * Return:int
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+/**
  * string_nconcat - a function that concatenates two strings
  * @s1 : string one
  * @s2 : string two
- * n : size of s2 to be printed
+ * @n : size of s2 to be printed
  * Return:character
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
-	int j;
-	int k;
-	int length;
+	unsigned int j = 0;
+	unsigned int k = 0;
 	char *p;
 
-	j = 0;
-	k = 0;
-	while (s1[i] != '\0')
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n < _strlen(s2))
 	{
-		length++;
-		i++;
+		p = malloc(_strlen(s1) + n + 1);
 	}
-	p = malloc(length + n + 1);
+	else
+		p = malloc(_strlen(s1) + _strlen(s2) + 1);
 
 	if (p == 0)
 		return (NULL);
@@ -31,13 +44,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[j] = s1[j];
 		j++;
 	}
-	while (n > 0)
+	while (s2[k] != '\0' && n > 0)
 	{
 		p[j] = s2[k];
 		n--;
 		k++;
 		j++;
 	}
-	p[j + 1] = '\0';
+	p[j] = '\0';
 	return (p);
 }
