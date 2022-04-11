657eac6f315cb1d0d2571a061b1507755ece753f
Samson <et980.0260@yahoo.com>
2022-04-11 11:46:27 -0700

    struct object of dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog object
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: dog name, age and owner
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif 
