4a90850307daad7cc7d4b03f9412896e5aa48d70
Samson <et980.0260@yahoo.com>
2022-04-18 06:18:18 -0700

    test for the project

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,22 +12,30 @@
 
 int main(int argc, char *argv[])
 {
-	int mul;
-	int arg1;
-	int arg2;
+	unsigned long mul;
+	int i, j;
 
 	if (argc == 3)
 	{
-		arg1 = atoi(argv[1]);
-		arg2 = atoi(argv[2]);
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] !='\0'; j++)
+			{
+				if (argv[i][j] > 57 || argv[i][j] < 48)
+				{
+					print("Error\n");
+					exit(98);
+				}
+			}
+		}
 
-		mul = arg1 * arg2;
-		printf("%d\n", mul);
+		mul = atol(argv[1]) * atol(argv[2]);
+		printf("%lu\n", mul);
 		return (0);
 	}
 	else
 	{
 		printf("Error\n");
-		return (1);
+		exit (98);
 	}
 }
