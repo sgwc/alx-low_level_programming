09954b52296f33364e19c7300ae5a1a457fb85d5
sgwc <45393676+sgwc@users.noreply.github.com>
2022-04-23 21:44:48 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	
 	size_t nodes;
+
 	nodes = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0 ,"(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
