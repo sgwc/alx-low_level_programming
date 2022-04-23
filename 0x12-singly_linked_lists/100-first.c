929edd817f5d8428182d11005f2258f7aa9fe8e8
sgwc <45393676+sgwc@users.noreply.github.com>
2022-04-23 22:24:57 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-void before_main() __attribute__((constructor));
-
 /**
  * before_main - prints before main
  *
  * Return: Nothing
  */
 
-void before_main()
+void __attribute__ ((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
