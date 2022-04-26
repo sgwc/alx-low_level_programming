10593b44112c31514727295cf24fbdef967683b2
Samson <et980.0260@yahoo.com>
2022-04-26 09:37:39 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,7 +7,7 @@
  * @head: head of the list
  * @indeX: index to insert at
  *
- * Return: the new node
+ * Return: 1 on success -1 if failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-	
+
 	next = prev->next;
 
 	if (index != 0)
