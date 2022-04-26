be0dc6c20e6aa095f6546848ce93d98b26250c92
Samson <et980.0260@yahoo.com>
2022-04-26 09:32:12 -0700

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_intdex - insert node at index
+ * @head: head of the list
+ * @indeX: index to insert at
+ *
+ * Return: the new node
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int nodes; /* counts nodes*/
+	listint_t *prev;
+	listint_t *next;
+
+	prev = *head;
+	if (index != 0)
+	{
+		for (nodes = 0; nodes < index - 1 && prev != NULL; nodes++)
+		{
+			prev = prev->next;
+		}
+	}
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
+	
+	next = prev->next;
+
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
+	return (1);
+}
