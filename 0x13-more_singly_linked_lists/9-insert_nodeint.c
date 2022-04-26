63316a80ecef17c08cd40195c7ebde3c75f00331
Samson <et980.0260@yahoo.com>
2022-04-26 11:50:50 -0700

    update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+
 	nodes = 0;
 	while (*head != NULL)
 	{
