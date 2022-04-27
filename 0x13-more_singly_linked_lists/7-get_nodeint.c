b9b27d7cfaf3b05d438ac9c370552285f70a8b7c
sgwc <45393676+sgwc@users.noreply.github.com>
2022-04-27 21:41:45 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,9 +12,9 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	
+
 	listint_t *temp = head;
-	
+
 	while (temp && count < index)
 	{
 		temp = temp->next;
