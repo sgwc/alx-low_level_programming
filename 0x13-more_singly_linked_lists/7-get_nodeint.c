8e1371faad99d74f2c82f4f402bf888d391b36d6
sgwc <45393676+sgwc@users.noreply.github.com>
2022-04-27 21:40:11 +0300

    Create 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (temp && count < index)
 	{
 		temp = temp->next;
-		i++;
+		count++;
 	}
 	return (temp ? temp : NULL);
 }
