932f837cb0279cb022ffb22b34ee4199b25014bf
Samson <et980.0260@yahoo.com>
2022-06-10 06:38:44 -0700

    update

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_dlistint - free list
+ * @head: head of the list
+ *
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+		{
+			head = head->prev;
+		}
+	}
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+			
+}
