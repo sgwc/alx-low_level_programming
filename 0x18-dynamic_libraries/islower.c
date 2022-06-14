bbfec326dd808f50a746a35d6454b7ff5ac319f8
sgwc <45393676+sgwc@users.noreply.github.com>
2022-06-14 06:09:38 +0300

    Update islower.c

--- 0x18-dynamic_libraries/islower.c
+++ 0x18-dynamic_libraries/islower.c
@@ -1,16 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _islower - checks the given charcter is in lower case
- * @c: chararcter to be checked
- *
- * Return: 0 if c is lower1, 1 if c is upper
- */
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
 int _islower(int c)
 {
-	if ('c' >= 97 && 'c' <= 122)
-		return (0);
-	else
-		return (1);
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
 }
