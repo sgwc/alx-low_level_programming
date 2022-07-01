2907a1fe4655a1861fea88a72fa6b90951ee7e19
Samson <et980.0260@yahoo.com>
2022-06-30 23:30:28 -0700

    create 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Function that creates a hash table
+ * @size: is the size of the array
+ * Return: a pointer to the newly created hash table or return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int counter;
+	hash_table_t *dict;
+
+	dict = malloc(sizeof(hash_table_t));
+	if (dict == NULL)
+		return (NULL);
+
+	dict->size = size;
+
+	dict->array = malloc(sizeof(hash_node_t *) * size);
+	if (dict->array == NULL)
+	{
+		return(NULL);
+	}
+	for (counter = 0; counter < size; counter++)
+	{
+		dict->array[counter] = NULL;
+	}
+	return (dict);
+}
